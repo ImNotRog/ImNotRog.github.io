13dd939e0a0a1e1ba1bf1d40117ac6a361f73034
Roger Fan <rogerjoeyfan@gmail.com>
2022-06-01 10:03:35 -0700

    Preliminaries

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<div>
+			<Head>
+				<title>Roger J. Fan</title>
+				<meta name="description" content="Hi! I'm Roger Fan, a student at Gunn High School's Class of 2024."></meta>
+			</Head>
+			
+			<Component {...pageProps} />
+		</div>
+	)
 }
 
 export default MyApp
